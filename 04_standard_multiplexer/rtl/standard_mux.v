ff4b9c1982403ad04dbe098b985486c5967cc795
takumasendeke <takumasendeke@gmail.com>
2026-07-17 00:16:57 +0200

    feat: used a multilexter to understard flow of operations

--- 04_standard_multiplexer/rtl/standard_mux.v
+++ 04_standard_multiplexer/rtl/standard_mux.v
@@ -7,17 +7,28 @@
 // By: Blaine C. Readler
 /////////////////////////////////////////////////////////////////////
 
-module simple_in_n_out // module/file name
+module standard_mux // module/file name
 (
     // Inputs
-
+    in_1,
+    in_2,
+    in_3,
 
     // Outputs
+    out_1;
 
 );
 
 // Port defenitions (I/O declartaions)
+input [3:0] in_1;
+input [3:0] in_2;
 
-// Design implementation 
+input in_3;
+
+output [3:0] out_1;
+
+// Design implementation
+assign out_1 = in_3 ? in_2: // standard mux in verilog (similar to switch/if statements in other languages)
+                      in_1; // defualt case 
 
 endmodule
